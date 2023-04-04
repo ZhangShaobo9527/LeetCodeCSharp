605e1ca9870981410b5aeb2109069098f2c4ac73
Zhang Shaobo <Zhang.Shaobo@outlook.com>
2023-04-04 12:24:34 +0800

    0002

---
+++ LeetCodeTests/SolutionsTest/_0002/SolutionTest.cs
@@ -0,0 +1,24 @@
+using LeetCodeSolutions.Definitions;
+using LeetCodeSolutions.Solutions._0002;
+using LeetCodeTests.TestHelper;
+using Xunit;
+
+namespace LeetCodeTests.SolutionsTest._0002;
+
+public class SolutionTest
+{
+    [Theory]
+    [InlineData("[2, 4, 3]", "[5, 6, 4]", "[7, 0, 8]")]
+    [InlineData("[0]", "[0]", "[0]")]
+    [InlineData("[9,9,9,9,9,9,9]", "[9,9,9,9]", "[8,9,9,9,0,0,0,1]")]
+    public void AddTwoNumbersTest(string l1Str, string l2Str, string expectedStr)
+    {
+        ListNode? actual = new Solution().AddTwoNumbers(
+            ListHelper.ParseLinkedListFromString(l1Str), 
+            ListHelper.ParseLinkedListFromString(l2Str));
+        Assert.Equal(
+            expected:expectedStr,
+            actual: ListHelper.Stringify(actual),
+            comparer: new StringifyListEqualityComparer());
+    }
+}
